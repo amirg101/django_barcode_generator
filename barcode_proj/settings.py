62ddc7b7d5c6861ff37ca63a0aac91ed8763d345
amirg101 <31856332+amirg101@users.noreply.github.com>
2021-05-27 22:44:02 +0530

    Update settings.py

--- barcode_proj/settings.py
+++ barcode_proj/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i(e&q(ybdo(zu&crrrt9rn0vmnfkdwjgg$gcp90@x!0f4$9dsb'
+SECRET_KEY = os.environ.get('SECRET_KEY',os.urandom(32))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -52,7 +52,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'barcode_proj.urls'
-import os 
 
 TEMPLATES = [
     {
